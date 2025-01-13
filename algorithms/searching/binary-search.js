7e59c7afdea57974c2bb33d1775e68785d9bfb16
Bittu <bittu1040@gmail.com>
2025-01-13 07:19:32 +0530

    added linear and binary search

---
+++ algorithms/searching/binary-search.js
@@ -0,0 +1,25 @@
+// always use sorted array as input
+
+function binarySearch(arr, target) {
+    let leftIndex = 0;
+    let rightIndex = arr.length - 1;
+
+    while (leftIndex <= rightIndex) {
+        const midIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+        if (arr[midIndex] === target) {
+            return midIndex;
+        }
+
+        if (arr[midIndex] < target) {
+            leftIndex = midIndex + 1;
+        }
+        else {
+            rightIndex = midIndex - 1;
+        }
+    }
+    return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 40))
+
